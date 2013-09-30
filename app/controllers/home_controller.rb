faa73852c08abc7f411b9e3768839f2d0bc08e64
LiwenL <lliu@metrostarsystems.com>
2013-09-30 15:10:19 -0400

    use new_messages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
     started = Branch.message_time_span.days.ago.to_s(:db)
     ended = Time.now.to_s(:db)
     @alerts = Stat.new(started, ended).alerted
-    @messages = Stat.new(started, ended).messages
+    @messages = Stat.new(started, ended).new_messages
     @calls = Stat.new(started, ended).number_of_calls
   end
 
