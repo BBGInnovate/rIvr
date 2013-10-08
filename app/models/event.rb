01e413474012a2506eaa209331254b9873305f75
LiwenL <lliu@metrostarsystems.com>
2013-10-08 09:08:32 -0400

    add after_save :update_health

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,24 @@
 class Event < ActiveRecord::Base
   belongs_to :action
   belongs_to :branch
+  
+  after_save :update_health
+  
   def to_label
     "Event Log"
   end
   def actions
     self.action
   end
+  
+  def update_health
+    h = self.branch.health
+    if h 
+      h.last_event = self.created_at
+      h.save
+    end
+  end
+  
   def self.truncate
     connection.execute "truncate table #{table_name}"
   end
